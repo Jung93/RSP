a34db1bbe32835fe4cce6049329c3ada2a7a4070
Jung93 <105581479+Jung93@users.noreply.github.com>
2025-03-21 18:38:37 +0900

    보스 에셋 추가 및 걷기 애니메이션 구현 3

---
+++ RSP/Source/RSP/Private/RSP_Boss.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RSP_Boss.h"
+
+ARSP_Boss::ARSP_Boss()
+{
+}
+
+void ARSP_Boss::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ARSP_Boss::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ARSP_Boss::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
---
+++ RSP/Source/RSP/Public/RSP_Companion.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RSP_Character.h"
+#include "RSP_Companion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RSP_API ARSP_Companion : public ARSP_Character
+{
+	GENERATED_BODY()
+	
+};
