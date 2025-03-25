6a1bf917e95bd4eaa62d6304f9cf6525d02d4a9f
Mhkans <harry45789@gmail.com>
2025-03-26 01:14:20 +0900

    기타 오류 수정
    위젯의 그림자 삭제
    아이템의 콜라이더를 캡슐에서 스피어로 변경
    아이템과 상점에 상호작용 추가(진행중)

--- RSP/Source/RSP/Item/RSP_ItemShop.cpp
+++ RSP/Source/RSP/Item/RSP_ItemShop.cpp
@@ -3,49 +3,72 @@
 
 #include "Item/RSP_ItemShop.h"
 #include "RSP_Player.h"
+#include "Blueprint/UserWidget.h"
+#include "UI/RSP_KeyPressEvent.h"
 #include "RSP_GameInstance.h"
-#include "Components/CapsuleComponent.h"
-
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 // Sets default values
 ARSP_ItemShop::ARSP_ItemShop()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
-	_collider = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Collider"));
+	_itemCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Collider"));
 	_mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 
-	_collider->SetupAttachment(_mesh);
+	_itemCollider->SetupAttachment(_mesh);
 	RootComponent = _mesh;
 
-	_collider->SetCollisionProfileName(TEXT("RSP_Item"));
-	_collider->SetRelativeLocation(FVector(0, 0, 44.0f));
-	//콜라이더의 크기는 에디터에서 조정
+	_itemCollider->SetCollisionProfileName(TEXT("RSP_Item"));
+	_itemCollider->SetRelativeLocation(FVector(0, 0, 44.0f));
+	//콜라이더의 크기는 에디터에서 조정 
+	_itemCollider->SetCollisionProfileName(TEXT("RSP_Item"));
 
-	//static ConstructorHelpers::FClassFinder<URSP_HpBar> hpBarClass(TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/Blueprint/UI/BP_RSP_HpBar.BP_RSP_HpBar_C'"));
-	//if (hpBarClass.Succeeded())
-	//{
-	//	_shopEnterWidget->SetWidgetClass(hpBarClass.Class);
-	//}
-	//
+	_shopEnterWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("Interaction"));
+	_shopEnterWidget->SetupAttachment(_mesh);
+	_shopEnterWidget->SetWidgetSpace(EWidgetSpace::World);
+
+	static ConstructorHelpers::FClassFinder<URSP_KeyPressEvent> hpBarClass(TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/Blueprint/UI/BP_RSP_KeyPressEvent.BP_RSP_KeyPressEvent_C'"));
+	if (hpBarClass.Succeeded())
+	{
+		_shopEnterWidget->SetWidgetClass(hpBarClass.Class);
+	}
+
 }
 
 // Called when the game starts or when spawned
 void ARSP_ItemShop::BeginPlay()
 {
 	Super::BeginPlay();
-	_collider->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::ColliderOverlapped);
-
+	_itemCollider->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::ColliderBeginOverlapped);
+	_itemCollider->OnComponentEndOverlap.AddDynamic(this, &ThisClass::ColliderEndOverlapped);
+	_shopEnterWidget->SetCastShadow(false);
 }
 
 // Called every frame
 void ARSP_ItemShop::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	auto playerCameraManager = GetWorld()->GetFirstPlayerController()->PlayerCameraManager;
 
+	if (playerCameraManager)
+	{
+		FVector widgetLocation = _shopEnterWidget->GetComponentLocation();
+		FVector cameraLocation = playerCameraManager->GetCameraLocation();
+		FRotator rotation = UKismetMathLibrary::FindLookAtRotation(widgetLocation, cameraLocation);
+		_shopEnterWidget->SetWorldRotation(rotation);
+	}
 }
 
-void ARSP_ItemShop::ColliderOverlapped(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void ARSP_ItemShop::ColliderBeginOverlapped(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	_shopEnterWidget->GetWidget()->SetVisibility(ESlateVisibility::Visible);
+}
+
+void ARSP_ItemShop::ColliderEndOverlapped(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	_shopEnterWidget->GetWidget()->SetVisibility(ESlateVisibility::Collapsed);
 }
 
