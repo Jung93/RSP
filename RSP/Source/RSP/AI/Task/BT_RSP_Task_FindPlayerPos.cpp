51ad63affaf1adcbafe8394e55820ff9cd86c705
Jung93 <105581479+Jung93@users.noreply.github.com>
2025-03-25 17:14:49 +0900

    콜리전 프로필 수정

---
+++ RSP/Source/RSP/AI/Task/BT_RSP_Task_FindPlayerPos.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Task/BT_RSP_Task_FindPlayerPos.h"
+
+#include "AI/RSP_AIController.h"
+#include "NavigationSystem.h"
+
+#include "RSP_Companion.h"
+#include "RSP_Player.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UBT_RSP_Task_FindPlayerPos::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Type result = Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	auto curPawn = OwnerComp.GetAIOwner()->GetPawn();
+
+	if (!curPawn->IsValidLowLevel())
+		return EBTNodeResult::Type();
+
+	FVector pos = curPawn->GetActorLocation();
+
+	auto player = Cast<ARSP_Player>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(FName(TEXT("Player"))));
+
+	if (!player->IsValidLowLevel())
+		return EBTNodeResult::Type::Failed;
+
+	FVector playerLocation = player->GetActorLocation();
+
+
+
+	OwnerComp.GetBlackboardComponent()->SetValueAsVector(FName(TEXT("RandomPos")), playerLocation * 1.2f);
+
+	return EBTNodeResult::Type::Succeeded;
+
+	//return EBTNodeResult::Type::Failed;
+}
