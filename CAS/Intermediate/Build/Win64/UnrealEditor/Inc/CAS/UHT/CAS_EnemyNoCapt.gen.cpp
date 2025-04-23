691e16ae7a6e7f4e2cb63946806b424b87466c85
Jung93 <105581479+Jung93@users.noreply.github.com>
2025-04-23 18:12:56 +0900

    Revert "초기 설정"
    This reverts commit e18d62cc66fd69c9df690d63b537d03b8687a6f3.

---
+++ CAS/Intermediate/Build/Win64/UnrealEditor/Inc/CAS/UHT/CAS_EnemyNoCapt.gen.cpp
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "CAS/Character/CAS_EnemyNoCapt.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeCAS_EnemyNoCapt() {}
+
+// Begin Cross Module References
+CAS_API UClass* Z_Construct_UClass_ACAS_Character();
+CAS_API UClass* Z_Construct_UClass_ACAS_EnemyNoCapt();
+CAS_API UClass* Z_Construct_UClass_ACAS_EnemyNoCapt_NoRegister();
+UPackage* Z_Construct_UPackage__Script_CAS();
+// End Cross Module References
+
+// Begin Class ACAS_EnemyNoCapt
+void ACAS_EnemyNoCapt::StaticRegisterNativesACAS_EnemyNoCapt()
+{
+}
+IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(ACAS_EnemyNoCapt);
+UClass* Z_Construct_UClass_ACAS_EnemyNoCapt_NoRegister()
+{
+	return ACAS_EnemyNoCapt::StaticClass();
+}
+struct Z_Construct_UClass_ACAS_EnemyNoCapt_Statics
+{
+#if WITH_METADATA
+	static constexpr UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+#if !UE_BUILD_SHIPPING
+		{ "Comment", "/**\n * \n */" },
+#endif
+		{ "HideCategories", "Navigation" },
+		{ "IncludePath", "Character/CAS_EnemyNoCapt.h" },
+		{ "ModuleRelativePath", "Character/CAS_EnemyNoCapt.h" },
+	};
+#endif // WITH_METADATA
+	static UObject* (*const DependentSingletons[])();
+	static constexpr FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<ACAS_EnemyNoCapt>::IsAbstract,
+	};
+	static const UECodeGen_Private::FClassParams ClassParams;
+};
+UObject* (*const Z_Construct_UClass_ACAS_EnemyNoCapt_Statics::DependentSingletons[])() = {
+	(UObject* (*)())Z_Construct_UClass_ACAS_Character,
+	(UObject* (*)())Z_Construct_UPackage__Script_CAS,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_ACAS_EnemyNoCapt_Statics::DependentSingletons) < 16);
+const UECodeGen_Private::FClassParams Z_Construct_UClass_ACAS_EnemyNoCapt_Statics::ClassParams = {
+	&ACAS_EnemyNoCapt::StaticClass,
+	"Game",
+	&StaticCppClassTypeInfo,
+	DependentSingletons,
+	nullptr,
+	nullptr,
+	nullptr,
+	UE_ARRAY_COUNT(DependentSingletons),
+	0,
+	0,
+	0,
+	0x009000A4u,
+	METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_ACAS_EnemyNoCapt_Statics::Class_MetaDataParams), Z_Construct_UClass_ACAS_EnemyNoCapt_Statics::Class_MetaDataParams)
+};
+UClass* Z_Construct_UClass_ACAS_EnemyNoCapt()
+{
+	if (!Z_Registration_Info_UClass_ACAS_EnemyNoCapt.OuterSingleton)
+	{
+		UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_ACAS_EnemyNoCapt.OuterSingleton, Z_Construct_UClass_ACAS_EnemyNoCapt_Statics::ClassParams);
+	}
+	return Z_Registration_Info_UClass_ACAS_EnemyNoCapt.OuterSingleton;
+}
+template<> CAS_API UClass* StaticClass<ACAS_EnemyNoCapt>()
+{
+	return ACAS_EnemyNoCapt::StaticClass();
+}
+ACAS_EnemyNoCapt::ACAS_EnemyNoCapt() {}
+DEFINE_VTABLE_PTR_HELPER_CTOR(ACAS_EnemyNoCapt);
+ACAS_EnemyNoCapt::~ACAS_EnemyNoCapt() {}
+// End Class ACAS_EnemyNoCapt
+
+// Begin Registration
+struct Z_CompiledInDeferFile_FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Character_CAS_EnemyNoCapt_h_Statics
+{
+	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
+		{ Z_Construct_UClass_ACAS_EnemyNoCapt, ACAS_EnemyNoCapt::StaticClass, TEXT("ACAS_EnemyNoCapt"), &Z_Registration_Info_UClass_ACAS_EnemyNoCapt, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ACAS_EnemyNoCapt), 675204284U) },
+	};
+};
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Character_CAS_EnemyNoCapt_h_3483134445(TEXT("/Script/CAS"),
+	Z_CompiledInDeferFile_FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Character_CAS_EnemyNoCapt_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Character_CAS_EnemyNoCapt_h_Statics::ClassInfo),
+	nullptr, 0,
+	nullptr, 0);
+// End Registration
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ CAS/Intermediate/Build/Win64/UnrealEditor/Inc/CAS/UHT/CAS_GameInstance.generated.h
@@ -0,0 +1,56 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Global/CAS_GameInstance.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CAS_CAS_GameInstance_generated_h
+#error "CAS_GameInstance.generated.h already included, missing '#pragma once' in CAS_GameInstance.h"
+#endif
+#define CAS_CAS_GameInstance_generated_h
+
+#define FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Global_CAS_GameInstance_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUCAS_GameInstance(); \
+	friend struct Z_Construct_UClass_UCAS_GameInstance_Statics; \
+public: \
+	DECLARE_CLASS(UCAS_GameInstance, UGameInstance, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/CAS"), NO_API) \
+	DECLARE_SERIALIZER(UCAS_GameInstance)
+
+
+#define FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Global_CAS_GameInstance_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UCAS_GameInstance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UCAS_GameInstance(UCAS_GameInstance&&); \
+	UCAS_GameInstance(const UCAS_GameInstance&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UCAS_GameInstance); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UCAS_GameInstance); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UCAS_GameInstance) \
+	NO_API virtual ~UCAS_GameInstance();
+
+
+#define FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Global_CAS_GameInstance_h_12_PROLOG
+#define FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Global_CAS_GameInstance_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Global_CAS_GameInstance_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Global_CAS_GameInstance_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CAS_API UClass* StaticClass<class UCAS_GameInstance>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_user_Unreal_RSP_RSP_CAS_Source_CAS_Global_CAS_GameInstance_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
